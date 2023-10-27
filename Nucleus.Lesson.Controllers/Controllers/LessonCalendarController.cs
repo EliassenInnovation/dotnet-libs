60e7f225b58aa7e690b2eabb592177aec8d7ab87
Connor Laverty <claverty@eliassen.com>
2023-10-27 08:33:11 +0100

    base

---
+++ Nucleus.Lesson.Controllers/Controllers/LessonCalendarController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nucleus.Lesson.Controllers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class LessonCalendarController
+    {
+    }
+}
